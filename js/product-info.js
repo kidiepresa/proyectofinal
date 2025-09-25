ce0b995d9ad95c285da9d5c81519b52619fde114
MatoGB <matiasg2955@gmail.com>
2025-09-25 12:03:57 -0300

    Productos relacionados

--- js/product-info.js
+++ js/product-info.js
@@ -2,8 +2,6 @@ function initProduct(){
 
     const productID = localStorage.getItem("prodID");
     const catID = localStorage.getItem("catID");
-    console.log(productID);
-    console.log(catID)
     fetch(`https://japceibal.github.io/emercado-api/cats_products/${catID}.json`)
     .then(response => response.json())
     .then(data => {
@@ -28,11 +26,61 @@ function showProduct(producto){
     
 }
 
+function showRelatedProducts(){
+    const relatedProducts = JSON.parse(localStorage.getItem("relatedProducts"));
+    const relatedContainer = document.getElementById("productos-relacionados");
+    relatedContainer.innerHTML = "";
+    console.log(relatedProducts);
+    const slides = Math.ceil((relatedProducts.length - 1) / 3);
+    let contador = relatedProducts.length - 1;
+    for (let i = 0; i < slides; i++) {
+        let div = document.createElement("div");
+        div.classList.add("carousel-item");
+        if (i === 0) {
+            div.classList.add("active");
+        }
+        let colDiv = document.createElement("div");
+        colDiv.classList.add("row", "justify-content-center");
+        for (let j = 0; j < 3; j++) {
+            if (contador >= 0) {
+                let producto = relatedProducts[contador];
+                console.log(relatedProducts[contador]);
+                console.log(i);
+                if (producto.id == localStorage.getItem("prodID")) {
+                    producto = relatedProducts[contador - 1];
+                    contador--;
+                }
+                if (contador >= 0) {
+                    if (producto.id != localStorage.getItem("prodID")) {
+                        let cardDiv = document.createElement("div");
+                        cardDiv.classList.add("col-12", "col-md-4", "d-flex", "justify-content-center");
+                        cardDiv.innerHTML = `
+                            <div class="card w-75">
+                                <img src="${producto.image}" class="card-img-top" alt="${producto.name}">
+                                <div class="card-body">
+                                    <h5 class="card-title">${producto.name}</h5>
+                                    <a href="product-info.html" class="btn btn-primary">Ver producto</a>
+                                </div>
+                            </div>
+                        `;
+                        colDiv.appendChild(cardDiv);
+                        div.appendChild(colDiv);
+                        contador--;
+                    }
+                }
+            }
+        }
+        relatedContainer.appendChild(div);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+
+initProduct();
+showRelatedProducts()
 
 
-
-
-
+})
 
 
 
